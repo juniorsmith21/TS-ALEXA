8a9791b3d860fcce72544fa3268e4f78c548f4a4
juniorsmith21 <ordonezjun0@gmail.com>
2026-04-21 23:34:19 -0500

    Ejercicio1

--- ejercicio1.ts
+++ ejercicio1.ts
@@ -1,1 +1,10 @@
-let hola={}
+function procesarNumero(valor: number): number {
+    return valor * 2;
+}
+
+function procesarTexto(valor: string): string {
+    return valor.toUpperCase();
+}
+
+console.log(procesarNumero(10));
+console.log(procesarTexto("ropa"));
