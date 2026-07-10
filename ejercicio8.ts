5b50ca0ed048f738f0277321f7fb86178dbd1a3d
juniorsmith21 <ordonezjun0@gmail.com>
2026-07-10 18:29:33 -0500

    cambio

--- ejercicio8.ts
+++ ejercicio8.ts
@@ -1,9 +1,9 @@
 //8.	Crea una función genérica que reciba un arreglo de cualquier
 //  tipo y retorne el último elemento. 
 
-function ultimoElemento<T>(arr: T[]): T {
-    return arr[arr.length - 1];
+function ultimoElement<A>(brr: A[]): A {
+    return brr[brr.length - 1];
 }
 
-console.log(ultimoElemento([1, 2, 3])); // 3
-console.log(ultimoElemento(["a", "b", "c"])); 
+console.log(ultElemento([1, 2, 3]));
+console.log(ultElemento(["a", "b", "c"]));
