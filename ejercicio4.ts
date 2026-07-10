da5e19519d8ca06a0ecf9990a5cfe687787efb2a
juniorsmith21 <ordonezjun0@gmail.com>
2026-07-10 18:19:03 -0500

    cambios

--- ejercicio4.ts
+++ ejercicio4.ts
@@ -1,16 +1,16 @@
 //Crea un arreglo de objetos usando una interfaz Producto (nombre, precio) 
 // y muestra solo los productos con precio mayor a 50. 
-interface Producto {
+interface Produc {
     nombre: string;
     precio: number;
 }
 
-const productos: Producto[] = [
+const productoso: Produc[] = [
     { nombre: "Camisa", precio: 30 },
     { nombre: "Zapatos", precio: 80 },
     { nombre: "Gorra", precio: 20 }
 ];
 
-const caros = productos.filter(p => p.precio > 50);
+const caroso = productoso.filter(p => p.precio > 50);
 
-console.log(caros);
+console.log(caroso);
